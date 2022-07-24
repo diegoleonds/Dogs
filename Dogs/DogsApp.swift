b17dfe07b767402d546c418a8059ba4fd9917bf9
Diego Santos <diegosantos@DiegoLeon-WP9YYHF0GV.local>
2022-07-23 23:57:12 -0300

    project created

---
+++ Dogs/DogsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogsApp.swift
+//  Dogs
+//
+//  Created by Diego Santos on 23/07/22.
+//
+
+import SwiftUI
+
+@main
+struct DogsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
